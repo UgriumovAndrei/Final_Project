633f488cab6606088a9c30f03d49c52f466a1281
ANDREI <ugryumovandrej@yandex.ru>
2022-12-29 20:14:39 +0300

    написал и добавил в репозиторий программу

---
+++ Programm/Program.cs
@@ -0,0 +1,24 @@
+﻿void PrintArray(string[]a)
+{
+    for(int i = 0; i< a.Length; i++) Console.Write($"{a[i]} ");
+}
+string [] SortArr(string [] arr)
+{
+    string [] array = new string [arr.Length];
+    for(int i = 0; i< arr.Length; i++)
+    {
+        if(arr[i].Length<=3)array[i] = arr[i];
+    }
+    return array;
+}
+Console.WriteLine("Сколько строковых значений будет в Вашем массиве?");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Заполните Ваш массив строками");
+string [] array  = new string [n];
+int i = 0;
+while (i < n)
+{
+    array[i] = Console.ReadLine();
+    i++;
+}
+PrintArray(SortArr(array));
